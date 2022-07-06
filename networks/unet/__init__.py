7cba99f1314fb7b4145b7218094f312f4a209f1b
dongsj <jkobekawh@gmail.com>
2022-07-06 12:24:31 +0800

    add nested unet

--- networks/unet/__init__.py
+++ networks/unet/__init__.py
@@ -1,3 +1,4 @@
 from networks.unet.unet import UNet
 from networks.unet.resunet import Res_UNet_50, Res_UNet_34, Res_UNet_101, Res_UNet_152
 from networks.unet.resdenseunet import Dense_UNet_121
+from networks.unet.nest_unet import NestedUNet
